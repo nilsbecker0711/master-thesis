845cff691cfe949c20220b14ffbc4d95314aeba6
Nils Becker <nils.becker0711@gmail.com>
2026-08-14 20:21:25 +0200

    bw adaptions

--- generator.sh
+++ generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p gpu_a100_short    # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
+#SBATCH -p dev_gpu_a100_il   # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
 #SBATCH -n 1                   # Number of tasks (1 for single node)
-#SBATCH -t 00:04:00            # Time limit (10 minutes for debugging purposes)
-#SBATCH --mem=40000        # Memory request (adjust as needed)
+#SBATCH -t 00:15:00            # Time limit (10 minutes for debugging purposes)
+#SBATCH --mem=400000        # Memory request (adjust as needed)
 #SBATCH --gres=gpu:1           # Request 1 GPU (adjust if you need more)
 #SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
 #SBATCH --ntasks-per-node=1    # Number of tasks per node (1 in this case)
@@ -22,7 +22,7 @@ module --ignore_cache load "cuda/11.8"
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate /pfs/work9/workspace/scratch/ma_nilbecke-thesis/miniconda3/envs/thesis_backup3
 export PYTHONNOUSERSITE=1
-export LD_LIBRARY_PATH=$/pfs/work9/workspace/scratch/ma_nilbecke-thesis/miniconda3/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pfs/work9/workspace/scratch/ma_nilbecke-thesis/miniconda3/lib
 export CS=/pfs/work9/workspace/scratch/ma_nilbecke-thesis/data/cityscapes
 export RES="--img_h 512 --img_w 1024"     # revisit after Phase 0.1
 export BASE="--arch segformer --cityscapes_root $CS"
@@ -32,9 +32,5 @@ python -c "import sys; print(sys.executable)"
 
 echo "Python version:"
 python --version
-#--lap_freeze_edges --lap_edge_thresh 0.15 --patch_scale 0.35
-python scripts/overfit.py --arch segformer --cityscapes_root "/pfs/work9/workspace/scratch/ma_nilbecke-thesis/data/cityscapes" \
-    --patch_mode lap --reference refs/street.png --loss_fn "cospgd" --image 3 \
-    --steps 300  --placement center \
-    --lap_alpha 0 --lap_beta 0 --placement_class 0 --tag "TEST DELETE" \
-    #--lap_freeze_edges --lap_edge_thresh 0.15 
+
+python scripts/train_conditional_generator.py --arch segformer --cityscapes_root "/pfs/work9/workspace/scratch/ma_nilbecke-thesis/data/cityscapes" --epochs 1 --train_images 128 --val_images 4 --panel_images "" --no_lpips --tag TIMING
