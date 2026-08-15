7ca227ce0190e6f3ef9687f862ba0ccd20f1da27
Nils Becker <nils.becker0711@gmail.com>
2026-08-15 10:18:58 +0200

    updated gen

--- generator.sh
+++ generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p dev_gpu_a100_il   # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
+#SBATCH -p gpu_a100_short    # Use the dev_gpu_4_a100 partition with A100 GPUs dev_gpu_4
 #SBATCH -n 1                   # Number of tasks (1 for single node)
 #SBATCH -t 00:15:00            # Time limit (10 minutes for debugging purposes)
-#SBATCH --mem=400000        # Memory request (adjust as needed)
+#SBATCH --mem=40000        # Memory request (adjust as needed)
 #SBATCH --gres=gpu:1           # Request 1 GPU (adjust if you need more)
 #SBATCH --cpus-per-task=16     # Number of CPUs per GPU (16 for A100)
 #SBATCH --ntasks-per-node=1    # Number of tasks per node (1 in this case)
@@ -33,4 +33,5 @@ python -c "import sys; print(sys.executable)"
 echo "Python version:"
 python --version
 
-python scripts/train_conditional_generator.py --arch segformer --cityscapes_root "/pfs/work9/workspace/scratch/ma_nilbecke-thesis/data/cityscapes" --epochs 1 --train_images 128 --val_images 4 --panel_images "" --no_lpips --tag TIMING
+#python scripts/train_conditional_generator.py --arch segformer --cityscapes_root "/pfs/work9/workspace/scratch/ma_nilbecke-thesis/data/cityscapes" --epochs 1 --train_images 128 --val_images 4 --panel_images "" --no_lpips --tag TIMING
+python scripts/train_conditional_generator.py --arch segformer --cityscapes_root "/pfs/work9/workspace/scratch/ma_nilbecke-thesis/data/cityscapes" --epochs 3 --train_images 512 --val_images 20 --gen_residual none --tag CONTROL
